97f7390caf7554785f2ba0f4f5d39fcdd0c1bcba
Aris <arisalexia3@gmail.com>
2024-03-16 23:44:12 +0100

    init

---
+++ drivelink-core/src/main/java/fr/eni/campus/drivelink/drivelinkApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.campus.drivelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class drivelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(drivelinkApplication.class, args);
+	}
+
+}
---
+++ drivelink-core/src/test/java/fr/eni/campus/drivelink/drivelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.campus.drivelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
